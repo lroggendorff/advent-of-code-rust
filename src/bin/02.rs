fc9da7b8b609f7cd773aaa8eaed36a11cc49eafb
Lance Roggendorff <lance.roggendorff@myemma.com>
2024-12-08 15:01:54 -0600

    Day 2 part 2.

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -37,8 +37,49 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(safe)
 }
 
+fn is_safely_increasing_pair(pair: &[i32]) -> bool {
+    let safely_increasing: Vec<i32> = (1..4).collect();
+    let difference = pair[1] - pair[0];
+    return safely_increasing.iter().any(|&v| difference == v)
+}
+
+fn is_safely_increasing_sequence(sequence: Vec<i32>) -> bool {
+    return sequence.windows(2).all(is_safely_increasing_pair);
+}
+
+fn is_safely_decreasing_pair(pair: &[i32]) -> bool {
+    let safely_decreasing: Vec<_>  = (-3..0).collect();
+    let difference = pair[1] - pair[0];
+    return safely_decreasing.iter().any(|&v| difference == v)
+}
+
+fn is_safely_decreasing_sequence(sequence: Vec<i32>) -> bool {
+    return sequence.windows(2).all(is_safely_decreasing_pair);
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut safe = 0;
+    for line in input.lines() {
+        let sequence: Vec<i32> = line.split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+
+        for i in 0..sequence.len() {
+            // for each item in sequence
+            let skipper = sequence
+                .clone()
+                .into_iter()
+                .enumerate()
+                .filter(|(j, _)| i != *j) // skip the item
+                .map(|(_, val)| val)
+                .collect::<Vec<i32>>();
+
+            if is_safely_increasing_sequence(skipper.clone()) || is_safely_decreasing_sequence(skipper.clone()) {
+                safe += 1;
+                break; // if the new sequence with a skipped value is safe, we're done
+            }
+        }
+    }
+
+    Some(safe)
 }
 
 #[cfg(test)]
@@ -52,8 +93,24 @@ mod tests {
     }
 
     #[test]
+    fn test_is_safely_increasing() {
+        let input = advent_of_code::template::read_file("examples", DAY);
+        let sequence: Vec<i32> = input.lines().last().unwrap().split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+        let result = is_safely_increasing_sequence(sequence);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn test_is_safely_decreasing() {
+        let input = advent_of_code::template::read_file("examples", DAY);
+        let sequence: Vec<i32> = input.lines().next().unwrap().split_whitespace().map(|s| s.parse::<i32>().unwrap()).collect();
+        let result = is_safely_decreasing_sequence(sequence);
+        assert_eq!(result, true);
+    }
+
+    #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
